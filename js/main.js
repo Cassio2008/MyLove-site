d4963c19c346e2e7ec7cded51a8f276ac7c5ca7c
Cassianoh-Rosa <ka98368337@gmail.com>
2026-04-18 15:24:37 -0300

    s

---
+++ js/main.js
@@ -0,0 +1,14 @@
+alert('Oiiii😁, aperta o botão ai, tem uma surpresa p oce')
+alert('Aperta dnv!')
+alert('pq vc não ta apertando?')
+alert('APERTA MAIS FORTEE')
+alert('Vc não quer ver a surpresa? aperta logo o botão 😔')
+alert('brincadeira kkkkkk a surpresa vai ser agr, só aperta mais uma vez')
+alert('só mais umazinha 😜')
+
+const img = document.querySelector('#foto')
+img.src = '/images/cachorro.jpg'
+
+const titu = document.querySelector('#title')
+titu.style.display = 'block'
+titu.innerHTML = 'Te adoro❤️'
